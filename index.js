6194a16a1f13dcf638cf09a0ec2c615f6eaf4ac6
nickyhuynh <nh.nicky.huynh@gmail.com>
2018-05-17 14:55:00 -0700

    ajax vote calls and express routing

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express'); //Express web server framework
+const uuid = require('uuid');
 
 const app = express();
 
 class Topic {
-  constructor(title) {
+  constructor(title, uuid) {
+    this.uuid = uuid;
     this.title = title;
     this.upvotes = 0;
     this.downvotes = 0;
   }
 }
 
-var topics = [];
+var topics = new Map();
+
+var id = uuid.v1();
+
+topics.set(id, new Topic("hello world", id))
+
+id = uuid.v1();
+
+topics.set(id, new Topic("mellow world", id));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -19,8 +29,31 @@ app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
   res.render('home.jade', {
-    topics: topics
+    topics: Array.from(topics.values())
   });
+});
+
+app.get('/submit', function(req, res) {
+});
+
+app.put('/upvote/:uuid', function(req, res) {
+  var uuid = req.params.uuid;
+
+  topics.get(uuid).upvotes += 1;
+
+  console.log(topics.get(uuid));
+
+  res.send(topics.get(uuid));
+});
+
+app.put('/downvote/:uuid', function(req, res) {
+  var uuid = req.params.uuid;
+
+  topics.get(uuid).downvotes += 1;
+
+  console.log(topics.get(uuid));
+
+  res.send(topics.get(uuid));
 });
 
 app.listen(8888);
